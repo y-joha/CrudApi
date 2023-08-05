1c22cbb24c9443a274e565d648a2afd8016a2a67
yohai.azulai <yohai28@gmail.com>
2023-08-06 00:33:45 +0300

    user good rider & part no working

--- Flaskkkk/app.py
+++ Flaskkkk/app.py
@@ -3,19 +3,34 @@ from flask_sqlalchemy import SQLAlchemy
 from os import environ
 from enum import Enum
 import datetime
+from sqlalchemy.dialects.postgresql import DOUBLE_PRECISION
 # to run this thing write this
 # sudo docker.compose up --build flask_app
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 db = SQLAlchemy(app)
 
-class BrandEnum(Enum):
+class System(Enum):
+    Fuel = 'Fuel'
+    Cooling = 'Cooling'
+    Engine_Top_End = 'Engine_Top_End'
+    Clutch = 'Clutch'
+    Engine_Lub = 'Engine_Lub'
+    Wheels_Tires = 'Wheels_Tires'
+    Final_Drive = 'Final_Drive'
+    Brakes = 'Brakes'
+    Suspension = 'Suspension'
+    Steering = 'Steering'
+    Electrical = 'Electrical'
+    Other = 'Other'
+
+class Brand(Enum):
     Kawasaki = 'Kawasaki'
     Suzuki = 'Suzuki'
     Yamaha = 'Yamaha'
     Honda = 'Honda'
 
-class Model_NameEnum(Enum):
+class Model_Name(Enum):
     Ninja = 'Ninja'
     Z = 'Z'
     
@@ -41,10 +56,10 @@ class Rider(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80),unique=True,nullable=False)
     email = db.Column(db.String(120),unique=True,nullable=False)
-    brand = db.Column(db.Enum(BrandEnum),nullable=False)
-    model = db.Column(db.Enum(Model_NameEnum),nullable=False)
+    brand = db.Column(db.Enum(Brand),nullable=False)
+    model = db.Column(db.Enum(Model_Name),nullable=False)
     km = db.Column(db.Integer, nullable=False)
-    year = db.Column(db.Date, nullable=False)
+    year = db.Column(db.DateTime, nullable=False)
     def json(self):
         return{
             'id' : self.id, 
@@ -53,7 +68,7 @@ class Rider(db.Model):
             'brand' : self.brand,
             'model' : self.model,
             'km' : self.km,
-            'year' : self.year.isoformat(),
+            'year' : self.year.isoformat()
         }
     
 class Tool(db.Model):
@@ -65,16 +80,29 @@ class Tool(db.Model):
     def json(self):
         return{'id' : self.id, 'name' : self.name}
     
-
+class Part(db.Model):
+    __tablename__ = 'parts'
+    
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(120),unique=True,nullable=False)
+    system=db.Column(db.Enum(System),unique=True,nullable=False)
+    torque=db.Column(db.Float,unique=True,nullable=False)
+    remarks=db.Column(db.String(20),unique=True,nullable=False)
+    part_makat=db.Column(db.Integer,unique=True,nullable=True)
+    
+    def json(self):
+        return{
+            'id' : self.id, 
+            'name' : self.name, 
+            'system' : self.system, 
+            'torque' : self.torque,
+            'remarks' : self.remarks,
+            'part_makat' : self.part_makat,
+            }
     
 db.create_all()
 
-
-#create a test route
-@app.route('/test', methods=['GET'])
-def test():
-    return make_response(jsonify({'message' : 'test route'}),200)
-
+#    RIDER METHODS *****************#
 #create a rider for the table
 @app.route('/riders', methods=['POST'])
 def create_rider():
@@ -92,8 +120,9 @@ def create_rider():
         db.session.commit()
         return make_response(jsonify({'message' : 'New Rider added to List'}), 201)
     except Exception as e:
-        return make_response(jsonify({'message': 'error creating new rider'}) , 500)
+        return make_response(jsonify({'message': 'Error creating new rider'}) , 500)
 
+#get all registered rides
 @app.route('/riders', methods=['GET'])
 def list_riders():
     try:
@@ -144,6 +173,36 @@ def delete_rider(id):
         return make_response(jsonify({'message' : 'rider not found'}),404)
     except Exception as e:
         return make_response(jsonify({'message' : 'error deleting rider'}),500)
+
+#    Parts METHODS *****************#
+#create part specific to model
+@app.route('/parts', methods=['POST'])
+def create_parts():
+    try:
+        data = request.get_json()
+        new_parts = Part(
+            name=data['name'],
+            system=data['system'],
+            torque=data['torque'],
+            remarks=data['remarks'],
+            part_makat=data['part_makat'],
+        )
+        db.session.add(new_parts)
+        db.session.commit()
+        return make_response(jsonify({'message' : 'New Part added to List'}), 201)
+    except Exception as e:
+        return make_response(jsonify({'message': 'error creating new part'}) , 500)
+
+#get all registered parts
+@app.route('/parts', methods=['GET'])
+def get_parts():
+    try:
+        parts = Part.query.all()
+        return make_response(jsonify([part.json() for part in parts]),200)
+    except Exception as e:
+        return make_response(jsonify({'message' :'error getting parts'}),500)
+
+#    User METHODS *****************#
 
 #create a user
 @app.route('/users', methods=['POST'])
